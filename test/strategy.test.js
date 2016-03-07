57ff117399a1758483765134d92b9e01b2b53a3c
fengyuhe <heyufeng8080@gmail.com>
2016-03-07 17:04:08 +0800

    add test
    add test

---
+++ test/strategy.test.js
@@ -0,0 +1,8 @@
+var headerStrategy = require('./lib/strategy');
+var should = require('should');
+
+describe('Strategy', function() {
+  it('should be return token', function(done){
+		done();
+	})
+});
